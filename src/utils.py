e2b94d01aab057a5f2383b93c5e5955217c65f62
PC1HA <ilia.popov.2015@gmail.com>
2025-03-11 19:13:02 +0300

    v6.0 доработан функционал классов, переделаны тесты, и утилс

--- src/utils.py
+++ src/utils.py
@@ -15,18 +15,18 @@ def load_data_from_json(file_path: str) -> List[Category]:
     categories: List[Category] = []
 
     try:
-        with open(file_path, 'r', encoding='utf-8') as file:
+        with open(file_path, "r", encoding="utf-8") as file:
             data: List[Dict[str, Any]] = json.load(file)
 
             for category_data in data:
-                category = Category(name=category_data['name'], description=category_data['description'])
+                category = Category(name=category_data["name"], description=category_data["description"])
 
-                for product_data in category_data['products']:
+                for product_data in category_data["products"]:
                     product = Product(
-                        name=product_data['name'],
-                        description=product_data['description'],
-                        price=product_data['price'],
-                        quantity=product_data['quantity']
+                        name=product_data["name"],
+                        description=product_data["description"],
+                        price=product_data["price"],
+                        quantity=product_data["quantity"],
                     )
                     category.add_product(product)
 
